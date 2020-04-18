762a208b6ea5cd60e7df04bc29f2e70f4b00ef33
rutujact <arutuj@gmail.com>
2020-04-17 19:22:02 -0600

    1. removed additional criteria on top ranking minimum negative likelihood based UDI to also have Rsq values above a threshold 2. As UDI is log-normally distributed calulated sd of log of top ranking UDI 3. Also added upper and lower UDI range at 2.5 and 97.5 quantiles

--- code/4.7.summarising_udi_by_sp_size.R
+++ code/4.7.summarising_udi_by_sp_size.R
@@ -3,7 +3,9 @@ gc()
 if (!require("pacman")) install.packages("pacman"); library(pacman)
 pacman::p_load(tidyverse, data.table)
 
-summarise.udi <- function(splevel, dryseason, rsq.thresh, root.selection, iso.subset, drop.months) {
+summarise.udi <- function(splevel = splevel, dryseason = dryseason, rsq.thresh = rsq.thresh,
+                          root.selection = root.selection, iso.subset = iso.subset,
+                          n.rank = n.rank, drop.months = drop.months) {
   load("results/GLUEsetup_part1_BCI.RData") # has model info and data on obs
   load(file.path("results/4.1GLUEsetup_part2_BCI.RData")) # has n.ensembles and growth and si matrix
 
@@ -16,10 +18,10 @@ summarise.udi <- function(splevel, dryseason, rsq.thresh, root.selection, iso.su
   dbh.residuals <- growth_by_si.info$dbh.residuals
   goodness.fit <- 0.3 # rsq0.3
   rsq.thresh <-  rsq.thresh
-  n.best.fit <- 100
   if (splevel == "on") {
     level.folder <- "splevel"
   } else {
+
     level.folder <- "commlevel"
   }
 
@@ -36,37 +38,58 @@ summarise.udi <- function(splevel, dryseason, rsq.thresh, root.selection, iso.su
     ds.bestfit.all <- ds.bestfit.all %>% subset(rf.sam %in% select.rf.sam$x)
   } # 702 root profiles selected
     ds.bestfit.select <- ds.bestfit.all %>%
-      #subset(!is.na(udi)) %>%
       group_by(sp_size) %>%
     arrange(sp_size, rsq) %>%
-    # need to find uncertainty in top-100 avaiable udi
-    mutate(rsq.rank = order(order(rsq, decreasing = TRUE)),
-           sdi.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, sdi),
-           udi.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, udi),
-           rsq.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, rsq),
-           root.95.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, root.95),
-           root.75.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, root.75),
-           max.root.tops = ifelse(rsq < rsq.thresh | rsq.rank > 500, NA, max.root),
-           udi.best = ifelse(rsq.rank == 1, udi, NA),
-           sdi.best = ifelse(rsq.rank == 1, sdi, NA)) %>%
-      subset(!is.na(rsq.tops)) %>%
-      arrange(sp_size, rsq.rank)
+    # need to find uncertainty in top n.rank avaiable udi
+      ## but UDI is not normally distributed but lognormally distributed
+    mutate(rsq.rank = rank(-rsq, ties.method = "average"),
+           ll.rank = rank(neg.likelihood, ties.method = "average"),
+           udi.best.rsq = ifelse(rsq.rank == 1, udi, NA),
+           sdi.best.rsq = ifelse(rsq.rank == 1, sdi, NA),
+           udi.best.ll = ifelse(ll.rank == 1, udi, NA),
+           sdi.best.ll = ifelse(ll.rank == 1, sdi, NA),
+           udi.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, udi),
+           sdi.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, sdi),
+           rsq.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, rsq),
+           udi.tops.ll = ifelse(ll.rank > n.rank, NA, udi),
+           sdi.tops.ll = ifelse(ll.rank > n.rank, NA, sdi),
+           rsq.tops.ll = ifelse(ll.rank > n.rank, NA, rsq),
+           root.95.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, root.95),
+           root.75.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, root.75),
+           max.root.tops.rsq = ifelse(rsq < rsq.thresh | rsq.rank > n.rank, NA, max.root),
+           root.95.tops.ll = ifelse(ll.rank > n.rank, NA, root.95),
+           root.75.tops.ll = ifelse(ll.rank > n.rank, NA, root.75),
+           max.root.tops.ll = ifelse(ll.rank > n.rank, NA, max.root)) %>%
+      subset(!is.na(rsq.tops.rsq)) %>%
+      arrange(sp_size, ll.rank, rsq.rank)
     ds.bestfit <- ds.bestfit.select %>%
-    summarise(udi.best = mean(udi.best, na.rm = TRUE),
-              udi = median(udi.tops, na.rm = TRUE), #sum(udi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
-              udi.sd = sd(udi.tops, na.rm = TRUE),
+    summarise(udi.best.rsq = mean(udi.best.rsq, na.rm = TRUE),
+              udi.best.ll = mean(udi.best.ll, na.rm = TRUE),
+              udi.med.rsq = median(udi.tops.rsq, na.rm = TRUE), #sum(udi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
+              udi.upr.rsq = quantile(udi.tops.rsq, probs = 0.95, na.rm = TRUE),
+              udi.lwr.rsq = quantile(udi.tops.rsq, probs = 0.05, na.rm = TRUE),
+              udi.sd.rsq = sd(log(udi.tops.rsq), na.rm = TRUE),
+              udi.med.ll = median(udi.tops.ll, na.rm = TRUE), #sum(udi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
+              udi.upr.ll = quantile(udi.tops.ll, probs = 0.95, na.rm = TRUE),
+              udi.lwr.ll = quantile(udi.tops.ll, probs = 0.05, na.rm = TRUE),
+              udi.sd.ll = sd(log(udi.tops.ll), na.rm = TRUE),
               n = n(),
-              udi.se = udi.sd/(n^0.5),
-              sdi.best = mean(sdi.best, na.rm = TRUE),
-              sdi = median(sdi.tops, na.rm = TRUE), ## sum(sdi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
-              sdi.sd = sd(sdi.tops, na.rm = TRUE),
-              sdi.se = sdi.sd/(n^0.5),
-              rsq.max = max(rsq.tops, na.rm = TRUE),
-              rsq.mean = mean(rsq.tops, na.rm = TRUE),
-              rsq.min = min(rsq.tops, na.rm = TRUE),
-              root.95 = median(root.95.tops, na.rm = TRUE),
-              root.75 = median(root.75.tops, na.rm = TRUE),
-              max.root = median(max.root.tops, na.rm = TRUE)) %>%
+              sdi.best.rsq = mean(sdi.best.rsq, na.rm = TRUE),
+              sdi.med.rsq = median(sdi.tops.rsq, na.rm = TRUE), ## sum(sdi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
+              sdi.sd.rsq = sd(log(sdi.tops.rsq), na.rm = TRUE),
+              sdi.best.ll = mean(sdi.best.ll, na.rm = TRUE),
+              sdi.med.ll = median(sdi.tops.ll, na.rm = TRUE), ## sum(sdi.tops*rsq.tops, na.rm = TRUE)/sum(rsq.tops, na.rm = TRUE),
+              sdi.sd.ll = sd(log(sdi.tops.ll), na.rm = TRUE),
+
+              root.95.rsq = median(root.95.tops.rsq, na.rm = TRUE),
+              root.75.rsq = median(root.75.tops.rsq, na.rm = TRUE),
+              max.root.rsq = median(max.root.tops.rsq, na.rm = TRUE),
+              rsq.max.ll = max(rsq.tops.ll, na.rm = TRUE),
+              rsq.mean.ll = mean(rsq.tops.ll, na.rm = TRUE),
+              rsq.min.ll = min(rsq.tops.ll, na.rm = TRUE),
+              root.95.ll = median(root.95.tops.ll, na.rm = TRUE),
+              root.75.ll = median(root.75.tops.ll, na.rm = TRUE),
+              max.root.ll = median(max.root.tops.ll, na.rm = TRUE)) %>%
     ungroup(sp_size) %>%
     separate(sp_size, c("sp", "size"), remove = FALSE) %>%
     mutate(size = factor(size, levels = c("tiny", "small", "medium", "large")))
@@ -85,11 +108,10 @@ summarise.udi <- function(splevel, dryseason, rsq.thresh, root.selection, iso.su
     subset(!is.na(udi))
   head(ds.bestfit.longer)
   summary(ds.bestfit.longer)
-  # if (splevel == "on") {
-  #   ds.bestfit$tlplevel <- "sp"
-  # } else {
-  #   ds.bestfit$tlplevel <- "comm"
-  # }
+
+
+  ds.bestfit.select <- ds.bestfit.select %>% mutate(udi.tops.rsq = list(udi.tops.rsq),
+                                                    udi.tops.ll = list(udi.tops.ll))
     if (splevel == "on") {
       ds.bestfit$tlplevel <- ds.bestfit.longer$tlplevel <- "sp"
     } else {

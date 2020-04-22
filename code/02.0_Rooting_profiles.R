c416001e7b690fbe271220614d31f00ca8668c1c
rutujact <arutuj@gmail.com>
2020-04-22 14:44:02 -0600

    added rooting profiles of the form in Zeng et al and used in ELM-FATES with parameters that can be specified in desired range

--- code/02.0_Rooting_profiles.R
+++ code/02.0_Rooting_profiles.R
@@ -26,13 +26,153 @@ theme_update(text = element_text(size=14),
              strip.background = element_blank()
 )
 
+file.path.figures <- file.path("figures", "rooting_profiles")
+if(!dir.exists(file.path.figures )) {dir.create(file.path.figures )}
 ####--------------------------------------------
-#### Rooting profiles for inversion
+####      Rooting profiles for inversion
 ####--------------------------------------------
+
+####--------------------------------------------
+## --------------in ELM-FATES-------------------
+####--------------------------------------------
+# FATES uses equation (2) in Zeng et al.
+## y = 1 - 0.5*(exp(-a*d) + exp(-b*d)) ....(eq 1)
+# where d = depth and Y = root fraction, a = fates_roota_par, b = fates_rootb_par
+
+# fates_roota_par: Parameter a in Eq. 2 in (Zeng 2001) that regulates the shape of the rooting profile.
+# Range 5.9 - 7.4 per m
+# Range corresponds to this parameter specified for BATS (or IGBP) land cover classified
+# Deciduous Broadleaf Trees (5.9 m-1) and Evergreen Broadleaf Trees  (7.4 m-1) as given in
+# Table 1 (or 2) of Zeng 2001.
+
+# fates_rootb_par : Parameter b in Eq. 2 in (Zeng 2001) that regulates the depth of the rooting profile.
+# Chosen range of b is derived using this equation so as to fit the observed range of rooting depth (dr)
+# of 2 - 18 m for Tropical Deciduous Forest (mean ± Standard Error (S.E.);
+# 3.7 ± 0.5, n = 5 trees; min = 2, max = 4.7) and
+# Tropical Evergreen Forest (mean ± S.E.; 7.3 ± 0.5, n = 3 trees and 3 communities; min = 2, max = 18 m)
+# combined (Canadell et al. 1996). Besides the direct observation of roots at 18 m included by (Nepstad et al. 1994)
+# in Paragominas, eastern Amazonia that is included in the above study;
+# in Tapajos, eastern Amazonia water extraction by roots was also inferred up to 18 m.
+# (Davidson et al. Forest Science 2011)
+
+# Rearranging eqn (1)
+# exp(-a*d) + exp(-b*d) = 2*(1 - Y)
+# exp(-b*d) = 2*(1 - Y) - exp(-a*d)
+# b = -log(2*(1 - Y) - exp(-a*d))/d
+# evaluate for Y = 0.99
+# For a = 5.9 and d = 18 m
+# exp(-b*18) = (1-0.99)*2 - exp(-5.9*18)
+a <- 5.9; Y <- 0.99
+d <- 18
+b <- -log((1 - Y)*2 - exp(-a*d))/d
+b
+# 0.2173346
+d <- 2
+b <- -log((1 - Y)*2 - exp(-a*d))/d
+# 1.956199
+# Therefore chosen range of b: 0.2173346 - 1.956199  per m
+
+####---------------------------------------------------------------------
+##++++++++++++++ New New Profiles with Zeng et al. form +++++++++++++++++
+####---------------------------------------------------------------------
+ ## range of b to be within available soil depths
 
 load(file = file.path("data-raw/psi.rda"))
 
 soil.depths <- unique(psi$depth)
+# For a = 7.4 and d = 18 m
+# For d = soil.depths[13]
+Y <- 0.99
+a <- 5.9;
+d <- soil.depths[13]
+b <- -log((1 - Y)*2 - exp(-a*d))/d
+b
+# at Y = 0.99, b = 0.3009248; at Y = 0.95, b = 0.1771219; a <- 5.9; d <- soil.depths[13]
+# at d = 1 m
+d <- soil.depths[5]
+a <- 19; # a needs to be exp(-a*d) - (1 - Y)*2 >= 0 for a difference of 0.01, exp(-a*d) = 0.01; a = -log(0.01)/d = 9.579037
+b <- -log((1 - Y)*2 - exp(-a*d))/d
+b
+# at Y = 0.99, b = 15.25082, a <- 11.1, d <- soil.depths[6]
+# at Y = 0.99, b = 12.46169, at Y = 0.95 b = , a <- 6.345685 , d <- soil.depths[7]
+# at Y = 0.95 b = 14.64649, a <- 6.345685, d <- soil.depths[6]
+#
+
+### new profiles -----
+
+# `a`` min, max are that from Zeng et al. for Broadleaf Deciduous and Evergreen.
+# `b` min max are such that Y=95 at d =  soil.depths[7] (~0.6 m) and soil.depths[13] (~ 13 m)
+
+all.info <- list(par.names = c("a", "b"),
+                 # min.param = c(5.9, 1.303), # as in Zeng et al for broad deci b 1.955 & broad evr bmin 1.303
+                 # max.param =  c(7.4, 1.956199))
+                 min.param = c(5.9, 0.3009248),
+                 max.param =  c(19, 30.96))
+#min.param = c(5.9, 0.2173346), ## for Y = 0.99 at d = soil.depths[13], b = 0.3026635 at a = 5.9
+#max.param =  c(7.4, 1.956)) # 7.4
+
+nsam <- 200
+
+## these should not be regenerated unless you really want to change a parameter of samples.
+## because each run will create new random combinations
+
+n.par <- length(all.info$par.names)
+grid <- lhs::randomLHS(n.sam, n.par)
+all.params <- matrix(0, n.sam, n.par)
+
+## generating ensembles
+for (i in 1: n.sam) {
+  for (j in 1: n.par) {
+    all.params[i, j] <- qunif(grid[i, j], min = all.info$min.param[j], max = all.info$max.param[j])
+  }
+}
+ab.tag <- paste0("ab_min_", paste0(all.info$min.param, collapse = "_"), "_max_", paste0(all.info$max.param, collapse = "_"))
+
+# rf.par <- read.csv(file = file.path(paste0("results/rf.sam_zeng_form_", ab.tag, "_", n.sam, ".csv")), header = TRUE)
+
+rf.par <- data.frame(rf.sam = rep(1:n.sam, each = length(soil.depths)),
+                     a = rep(all.params[, 1], each = length(soil.depths)),
+                     b = rep(all.params[, 2], each = length(soil.depths)),
+                     depth = rep(soil.depths, times = n.sam))
+rf.par <- rf.par %>%
+  mutate(cum.root.frac = 1 - 0.5*(exp(-a*depth) + exp(-b*depth))) %>%
+  arrange(rf.sam, depth) %>%
+  group_by(rf.sam) %>%
+  mutate(root.frac = cum.root.frac - lag(cum.root.frac, default = NA),
+         root.frac = ifelse(is.na(root.frac), 0, root.frac)) %>% ungroup(rf.sam) %>%
+  arrange(rf.sam, depth)
+summary(rf.par)
+# max(rf.par$cum.root.frac[rf.par$depth == soil.depths[7]])
+# 0.9999949 ab_min_5.9_0.3009248_max_19_30.96
+
+# write.csv(rf.par, file.path(paste0("results/rf.sam_zeng_form_", ab.tag, "_", n.sam, ".csv")), row.names = FALSE)
+
+g1 <- ggplot(data = rf.par, aes(y = depth, x = root.frac)) +
+  xlab("Root fraction") +
+  scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2),2)) #+
+g1.1 <- g1 + geom_point(aes(group = rf.sam, color = as.factor(rf.sam)), show.legend = FALSE)
+ggsave(file.path(file.path.figures, paste0("points_rooting_profiles_for_inversion_ELM_FATES_depths_zeng_", ab.tag, ".jpeg")),
+       plot = g1.1, height = 6.5, width = 5, units = 'in')
+
+g1.2 <- g1 + geom_path(aes(group = rf.sam, color = as.factor(rf.sam)), show.legend = FALSE)
+ggsave(file.path(file.path.figures, paste0("path_rooting_profiles_for_inversion_ELM_FATES_depths_zeng_", ab.tag, ".jpeg")),
+         plot = g1.2, height = 6.5, width = 5, units = 'in')
+
+g2 <- ggplot(data = rf.par, aes(y = depth, x = cum.root.frac)) +
+  xlab("Cumulative Root Fraction") +
+  geom_line(aes(group = rf.sam, color = as.factor(rf.sam)), show.legend = FALSE) +
+  # scale_colour_discrete(name = "Depth at \nCumulative Root Fraction = 0.9999999") +
+  scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2),2)) +
+  # theme(legend.position = c(0.4, 0.5)) +
+  scale_x_continuous(breaks = seq(0.1, 1, by = 0.1)) #+
+ggsave(file.path(file.path.figures, paste0("cumulative_rooting_profiles_for_inversion_ELM_FATES_depths_zeng_", ab.tag, ".jpeg")),
+       plot = g2, height = 6.5, width = 5, units ='in')
+
+
+####----------------------------------------------------------------
+##++++++++++++++ End of New Profiles with Zeng et al. ++++++++++++++
+####----------------------------------------------------------------
+
 # max.rds.df <- data.frame(max.rds = soil.depths[-length(soil.depths)]) %>%
 #   mutate(lag.max.rds = lag(max.rds, default = 0),
 #          diffby2 = (max.rds - lag.max.rds)/2,
@@ -87,21 +227,21 @@ ggplot(profiles %>% subset(maxD == max.rds[length(max.rds)]), aes(y = abs.depth,
   ylab("Depths (m)") + xlab("Cumulative Root Fraction (0-1)") +
   geom_line(aes(group = rf.sam)) +
   scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_one depth.jpeg")), height = 8, width = 8, units ='in')
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_one depth.jpeg")), height = 8, width = 8, units ='in')
 
 ggplot(profiles,
        aes(y = abs.depth, x = cum.root.frac, color = as.factor(rf.sam))) +
   ylab("Depths (m)") + xlab("Cumulative Root Fraction (0-1)") +
   geom_line(aes(group = rf.sam), alpha = 0.7, show.legend = FALSE) +
   scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion.jpeg")), height = 8, width = 8, units ='in')
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion.jpeg")), height = 8, width = 8, units ='in')
 
 ggplot(profiles %>% subset(!is.na(depth)),
        aes(y = depth, x = cum.root.frac, color = as.factor(rf.sam))) +
   ylab("Depths (m)") + xlab("Cumulative Root Fraction (0-1)") +
   geom_line(aes(group = rf.sam), alpha = 0.7, show.legend = FALSE) +geom_point(show.legend = FALSE) +
   scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_ELM_FATES_depths.jpeg")), height = 8, width = 8, units ='in')
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_ELM_FATES_depths.jpeg")), height = 8, width = 8, units ='in')
 
 ### only those rf.sam in which root fraction does not increase with depth
 
@@ -110,7 +250,7 @@ ggplot(profiles %>% subset(power < 0.5),
   ylab("Depths (m)") + xlab("Cumulative Root Fraction (0-1)") +
   geom_line(aes(group = rf.sam), alpha = 0.7, show.legend = FALSE) +
   scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_power.threshold_0.5.jpeg")), height = 8, width = 8, units ='in')
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_power.threshold_0.5.jpeg")), height = 8, width = 8, units ='in')
 
 ggplot(profiles %>% subset(!is.na(depth) & power < 0.5),
        aes(y = depth, x = cum.root.frac, color = as.factor(rf.sam))) +
@@ -118,7 +258,7 @@ ggplot(profiles %>% subset(!is.na(depth) & power < 0.5),
   geom_line(aes(group = rf.sam), alpha = 0.7, show.legend = FALSE) +
   geom_point(show.legend = FALSE) +
   scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_ELM_FATES_depths_power.threshold_0.5.jpeg")), height = 8, width = 8, units ='in')
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_ELM_FATES_depths_power.threshold_0.5.jpeg")), height = 8, width = 8, units ='in')
 
 ### creating new profiles that exponentially decrease
 no.of.bases <- 1000
@@ -149,7 +289,7 @@ ggplot(base.df, aes(x = x, y = base)) +
   annotate("text", x = 100, y = 0.1*max(base.df$base, na.rm = TRUE), parse = TRUE, label =
              as.character(expression("Base = e"^x)),
             family = "Helvetica", size = 6)
-ggsave(file.path(paste0("figures/base_exponetially_increasing.jpeg")), height = 6, width = 6, units ='in')
+ggsave(file.path(file.path.figures, paste0("base_exponetially_increasing.jpeg")), height = 6, width = 6, units ='in')
 
 pro.df <- data.frame(depth = rep(soil.depths, times = length(base.df$base)),
                       base = rep(base.df$base, each = length(soil.depths)),
@@ -175,7 +315,7 @@ p.rf <- ggplot(pro.df, aes(y = depth, x = root.frac)) +
            family = "Helvetica", size = 5) +
   scale_y_continuous(trans="rev_sqrt", breaks = rf.breaks, labels = rf.labels)
   # scale_y_reverse(breaks = rf.breaks, labels = rf.labels)  # scale_y_continuous(trans="rev_sqrt", breaks = signif(round(soil.depths, 2), 2))
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_exponentially_decreasing_root.frac.jpeg")),
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_exponentially_decreasing_root.frac.jpeg")),
        plot = p.rf, height = 6, width = 4.5, units ='in')
 
 p.cum.rf <- ggplot(pro.df, aes(y = depth, x = cum.root.frac)) +
@@ -190,7 +330,7 @@ p.cum.rf <- ggplot(pro.df, aes(y = depth, x = cum.root.frac)) +
            family = "Helvetica", size = 5) +
   # scale_y_reverse(breaks = rf.breaks, labels = rf.labels)
   scale_y_continuous(trans="rev_sqrt", breaks = rf.breaks, labels = rf.labels)
-ggsave(file.path(paste0("figures/rooting_profiles_for_inversion_exponentially_decreasing.jpeg")),
+ggsave(file.path(file.path.figures, paste0("rooting_profiles_for_inversion_exponentially_decreasing.jpeg")),
        plot = p.cum.rf, height = 6, width = 4.5, units ='in')
 
 write.csv(pro.df, "results/rf.sam_exponentially_decreasing.csv", row.names = FALSE)

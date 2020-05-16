d092389e3310c4cd117c0ab23795b9f060d962a8
rutujact <arutuj@gmail.com>
2020-05-16 10:49:42 -0600

    PSI distribution added

--- code/04.1_distribution_qqplots_growth_rate_inverse_model_residuals_UDI.R
+++ code/04.1_distribution_qqplots_growth_rate_inverse_model_residuals_UDI.R
@@ -9,7 +9,9 @@ rm(list = ls())
 
 gc()
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, gridExtra)
+p_load(tidyverse, gridExtra, bci.elm.fates.hydro, spatstat)
+figures.folder <- paste0("figures/Distributions")
+if(!dir.exists(file.path(figures.folder))) {dir.create(file.path(figures.folder))}
 
 ### Growth rates of dbh residuals
 
@@ -17,14 +19,16 @@ load(file.path("results/GLUEsetup_part2_BCI.RData"))
 growth.rate <- as.vector(unlist(growth_by_si.info$growth))
 qex <- function(x) qexp((rank(x)-.375)/(length(x)+.25))
 
-jpeg(paste0("figures/growth_rates_distribution_dbh.residuals_", growth_by_si.info$dbh.residuals, ".jpeg"),
+jpeg(file.path(figures.folder, "growth_rates_distribution_dbh.residuals_", growth_by_si.info$dbh.residuals, ".jpeg"),
      width = 960, height = 960, units = "px", pointsize = 24,
      quality = 100)
 par(mfrow = c(2, 2))
-qqnorm(growth.rate, main = "Normal Q-Q plot: Growth Rate")
-plot(qex(growth.rate), growth.rate)
+qqnorm(growth.rate, main = "Normal Q-Q plot: Growth Residuals")
+plot(qex(growth.rate), growth.rate, main = "Exponential Q-Q plot: Growth Rate",
+     ylab = "Growth Residuals", xlab = "Exponential Quantiles")
 qqnorm(log(growth.rate), main = "Normal Q-Q plot: log(Growth Rate)")
-plot(qex(growth.rate), log(growth.rate))
+plot(qex(growth.rate), log(growth.rate), main = "Exponential Q-Q plot: log(Growth Rate)",
+     ylab = "Log(Growth Residuals)", xlab = "Exponential Quantiles")
 dev.off()
 ks.test(growth.rate, "plnorm", alternative = c("two.sided"), exact = FALSE)
 shapiro.test(sample(log(growth.rate), 5000))
@@ -32,8 +36,8 @@ shapiro.test(sample(log(growth.rate), 5000))
 load("/Users/rutuja/Work_at_LANL/Projects/bci.roots.inversion/results/splevel/GLUE.resid_drop.monthsNone_cor0.3_2019-10-14_5000_0_med_size_class_predefined_cc_scaled_on_5.Rdata")
 resid.growth.all <- as.numeric(unlist(GLUE.resid))
 resid.growth <- sample(resid.growth.all, 10000)
-jpeg("figures/residual_growth_rates_distribution.jpeg", width = 960, height = 960, units = "px", pointsize = 24,
-     quality = 100)
+jpeg(file.path(figures.folder, "residual_growth_rates_distribution.jpeg", width = 960, height = 960, units = "px", pointsize = 24,
+     quality = 100))
 par(mfrow = c(2, 2))
 qqnorm(resid.growth, main = "Normal Q-Q plot: Residuals")
 plot(qex(resid.growth), resid.growth)
@@ -68,3 +72,81 @@ for (i in 1:10) {
   shapiro.test(sample(udi.sp, 5000))
   ks.test(sample(udi.sp, 5000), "plnorm", alternative = c("two.sided"), exact = FALSE)
 }
+
+###---------------
+### PSI
+###---------------
+psi.mean <- bci.elm.fates.hydro::psi.mean
+hist(psi.mean$psi)
+shapiro.test(sample(psi.mean$psi, 5000))
+ks.test(sample(psi.mean$psi, 5000), "plnorm", alternative = c("two.sided"), exact = FALSE)
+
+swp <- -psi.mean$psi
+
+# https://daviddalpiaz.github.io/stat3202-sp19/notes/fitting.html
+jpeg(file.path(figures.folder, "psi_distribution.jpeg"), width = 900, height = 600, units = "px", pointsize = 24,
+     quality = 80)
+par(mfrow = c(2, 3))
+## Exp
+hist(swp, probability = TRUE,
+     main = "SWP", xlab = "SWP (-MPa)")
+box()
+grid()
+curve(dexp(x, rate = 1 / mean(swp)),
+      add = TRUE, col = "darkorange")
+
+
+##gamma# calculating sample moments
+len_samp_moment_1 = mean(swp)
+len_samp_moment_2 = mean(swp ^ 2)
+
+# method of moments estimators
+len_alpha_mom = len_samp_moment_1 ^ 2 / (len_samp_moment_2 - len_samp_moment_1 ^ 2)
+len_beta_mom  = len_samp_moment_1 / len_alpha_mom
+
+# estimates for this dataset
+c(len_alpha_mom, len_beta_mom)
+
+hist(swp, probability = TRUE,
+     main = "SWP", xlab = "SWP (-MPa)")
+box()
+grid()
+curve(dgamma(x, shape = len_alpha_mom, scale = len_beta_mom),
+      add = TRUE, col = "darkorange", lwd = 2)
+## kernel density
+hist(swp, probability = TRUE,
+     main = "SWP", xlab = "SWP (-MPa)")
+box()
+grid()
+lines(density(swp), col = "darkorange")
+
+## exp qq plot
+qqplot(x = qexp(ppoints(swp), rate = 1 / mean(swp)),
+       y = swp,
+       # xlim = c(0, 400), ylim = c(0, 400),
+       main = "QQ-Plot: SWP, Exp Distribution",
+       xlab = "Theoretical Quantiles, Exp Distribution",
+       ylab = "Sample Quantiles, SWP")
+abline(a = 0, b = 1, col = "dodgerblue", lwd = 2)
+# gamma qq
+qqplot(x = qgamma(ppoints(swp),
+                  shape = len_alpha_mom, scale = len_beta_mom),
+       y = swp,
+       # xlim = c(0, 400), ylim = c(0, 400),
+       main = "QQ-Plot: SWP, Gamma Distribution",
+       xlab = "Theoretical Quantiles, Gamma Distribution",
+       ylab = "Sample Quantiles, SWP")
+abline(a = 0, b = 1, col = "dodgerblue", lwd = 2)
+grid()
+## kernel qq
+qqplot(x = quantile.density(density(swp), ppoints(swp)),
+       y = swp,
+       # xlim = c(0, 400), ylim = c(0, 400),
+       main = "QQ-Plot: SWP, KDE",
+       xlab = "Theoretical Quantiles, Kernel Density Estimate",
+       ylab = "Sample Quantiles, SWP")
+abline(a = 0, b = 1, col = "dodgerblue", lwd = 2)
+grid()
+dev.off()
+## So none of the models fit, using non-parametric Kernel Density Estimator
+

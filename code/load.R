9535d905c7a75076cef362aca0d492510a5b15d1
rutujact <arutuj@gmail.com>
2020-07-14 20:01:40 -0600

    Only loads data; moved other stuff to a new file

--- code/load.R
+++ code/load.R
@@ -4,11 +4,22 @@
 #*******************************************
 
 if (!require("pacman")) install.packages("pacman"); library(pacman)
-pacman::p_load(tidyverse, readxl, forcats, scales, data.table)
+pacman::p_load(tidyverse, readxl, forcats, scales, data.table, ggpmisc)
 
+# graphics info
+theme_set(theme_bw())
+theme_update(text = element_text(size = 14),
+             panel.grid.major = element_blank(),
+             panel.grid.minor = element_blank(),
+             strip.background = element_blank()
+)
+#******************************************************
+## folders
+#******************************************************
+figures.folder <- paste0("figures/PhenoDemoTraitsPsi")
+if(!dir.exists(file.path(figures.folder))) {dir.create(file.path(figures.folder))}
 results.folder <- paste0("results/PhenoDemoTraitsPsi")
 if(!dir.exists(file.path(results.folder))) {dir.create(file.path(results.folder))}
-
 #******************************************************
 ## Load Deciduousness-----
 #******************************************************
@@ -33,10 +44,10 @@ load(file = file.path(results.folder, "hyd.traits.all.RData"))
 load(file = file.path(results.folder, "kunert.traits.all.RData"))
 load(file = file.path(results.folder, "hyd.traits.key.long.RData"))
 load(file = file.path(results.folder, "kunert.traits.key.long.RData"))
+
 #******************************************************
 ## Load Isotopic data-----
 #******************************************************
-
 load(file = "data-raw/traits/isotopes/Oecologia 1995 Jackson_Fig3_Fig4_& Meinzer 1999_Fig4.Rdata")
 leafless_mar.apr <- read.csv("data-raw/traits/isotopes/Meinzer_1999_isotope_sp_leafless_in_mar_april.csv")
 # load(file = "results/all_isotopic_record.Rdata")
@@ -76,12 +87,14 @@ load(file = file.path(results.folder, "psi.interpolated.depths_layers_combined.R
 #******************************************************
 load(file = file.path(results.folder, "clim.daily_with_pet.PM.Rdata"))
 load(file = file.path(results.folder, "gpp.models.Rdata"))
+
 #******************************************************
 ### Load K by Psi models -------
 #******************************************************
 load(file = file.path(results.folder, "k_by_psi.models.Rdata"))
 load(file = file.path(results.folder, "data.model.AB.Rdata"))
 load(file = file.path(results.folder, "sp.soft.filled.Rdata"))
+
 #******************************************************
 ### Load Leaf Cohort tracking data from the crane sites------
 #******************************************************
@@ -90,17 +103,3 @@ load(file = file.path(results.folder, "sp.leaf_cover.for.model.Rdata"))
 
 load(file = file.path(results.folder, "cohort.Rdata"))
 load(file = file.path(results.folder, "coh.sp.summ.Rdata"))
-
-
-# #******************************************************
-# ## output
-# #******************************************************
-load(file = file.path(results.folder, "ml.rsq.combine.best.Rdata"))
-###
-ml.rsq.combine.best <- ml.rsq.combine.best %>% left_join(bci.traits %>% dplyr::select(sp, form1), by = "sp") %>%
-  mutate(depth = as.numeric(depth))
-erd.data <- ml.rsq.combine.best %>%
-  subset(corr >= 0 & form1 == "T" & sp != "guapst" &
-           corr.func == "gr.Psi.VPD" & source == "Meinzer et al.1999 Fig. 4" &
-           R2 >= 0.1)
-

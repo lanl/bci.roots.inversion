588ec65c9bd8f321c22c8dbbc4a0e3669f69a8f8
rutujact <arutuj@gmail.com>
2020-08-04 11:28:03 -0600

    edited units

--- code/load.R
+++ code/load.R
@@ -104,6 +104,7 @@ load(file = file.path(results.folder, "sp.leaf_cover.for.model.Rdata"))
 load(file = file.path(results.folder, "cohort.Rdata"))
 load(file = file.path(results.folder, "coh.sp.summ.Rdata"))
 
+soil.depths <- unique(psi$depth)
 #******************************************************
 ### Supporting data referenced in the manuscript
 #******************************************************
@@ -200,30 +201,30 @@ param.table <-
       ),
     Units =
       c(
-        "Unitless",
-        "$m^2gC^{-1}$",
-        "umol $CO^2m^2s^{-1}$",
-        "per m",
-        "$m^{-1}$",
+        "unitless",
+        "m^2^gC^-1^",
+        "umol CO^2^m^2^s^-1^",
+        "m^-1^",
+        "m^-1^",
         "mm",
         "m",
         "unitless",
         "unitless",
-        "$mms^{-1}$",
+        "mms^-1^",
         "unitless"
       ),
     `Method of determination with reference` =
       c(
         "The range of Ball Berry parameter fitted across several studies as compiled in Table 2 of [@Medlyn:2012]",
-        "Based on the observed range of Leaf Mass Area (LMA)--24.97 to 235.8 $gm^{-2}$-- for individual tree variation across 51 tree species in Barro Colorado Island (this study), and assumption of 50% carbon content in biomass.",
+        "Based on the observed range of Leaf Mass Area (LMA)--24.97 to 235.8 gm^-2^-- for individual tree variation across 51 tree species in Barro Colorado Island (this study), and assumption of 50% carbon content in biomass.",
         "Observed range of $V_{cmax}$ at  $25^\\circ$C for tropical tree species under relative radiation of 50% or greater, thereby excluding highly shaded leaves. Values derived with a conversion specific for CLM model are used. [@Ali:2015]",
-        "Parameter a in Eq. 2 in [@Zeng:2001] that regulates the shape of the rooting profile. Range corresponds to this parameter specified for BATS (or IGBP) land cover classified Deciduous Broadleaf Trees (5.9 $m^{-1}$ and Evergreen Broadleaf Trees  (7.4 $m^{-1}$) as given in Table 1 of [@Zeng:2001].",
-        "Parameter b in Eq. 2 in [@Zeng:2001] that regulates the depth of the rooting profile. Chosen range of *b* is derived using this equation so as to fit the observed range of rooting depth (d~r~) of 2 - 18 m for Tropical Deciduous Forest (mean ± Standard Error (SE); 3.7 ± 0.5, *n* = 5 trees; min = 2, max = 4.7)  and Tropical Evergreen Forest (mean $\\pm$ S.E.; 7.3 $\\pm$ 0.5, n = 3 trees and 3 communities; min = 2, max = 18 m) combined [@Canadell:1996]. Besides the direct observation of roots at 18 m included by [Nepstad:1994] in Paragominas, eastern Amazonia that is included in the above study; in Tapajos, eastern Amazonia water extraction by roots was also inferred up to 18 m. [@Davidson:2011]",
+        "Parameter a in Eq. 2 in [@Zeng:2001] that regulates the shape of the rooting profile. Range corresponds to this parameter specified for BATS (or IGBP) land cover classified Deciduous Broadleaf Trees (5.9 m^-1^ and Evergreen Broadleaf Trees  (7.4 m^-1^) as given in Table 1 of [@Zeng:2001].",
+        "Parameter b in Eq. 2 in [@Zeng:2001] that regulates the depth of the rooting profile. Chosen range of *b* is derived using this equation so as to fit the observed range of rooting depth (d~r~) of 2 - 18 m for Tropical Deciduous Forest (mean ± Standard Error (SE); 3.7 ± 0.5, *n* = 5 trees; min = 2, max = 4.7)  and Tropical Evergreen Forest (mean $\\pm$ S.E.; 7.3 $\\pm$ 0.5, n = 3 trees and 3 communities; min = 2, max = 18 m) combined [@Canadell:1996]. Besides the direct observation of roots at 18 m included by [@Nepstad:1994] in Paragominas, eastern Amazonia that is included in the above study; in Tapajos, eastern Amazonia water extraction by roots was also inferred up to 18 m. [@Davidson:2011]",
         "Based on observed range of -1.13 to -2.42 MPa for leaf turgor loss point for individual tree variation across 49 BCI tree species. This study.",
         "Ben Turner; pers. comm.",
         "Empirical",
         "To account for high macroporosity and direct flow paths in tropical soils that is not accounted for by small soil core samples [@Broedel:2017; @Kinner:2004; @Tomasella:1998]",
-        "For Conrad catchment observed median Ksat (95% CI) for 12.5 cm depth is 38.3 $mmhr^{-1}$ (25.4 - 51.2, n = 75), while for 60 cm depth 0.7 $mmhr^{-1}$ (0.2 - 1.2, n = 40) [@Kinner:2004]. For reference, a storm with 12.5 mm/hr rainfall intensity has a 0.2 probability of occuring in any given rainfall event.",
+        "For Conrad catchment observed median Ksat (95% CI) for 12.5 cm depth is 38.3 mmhr^-1^ (25.4 - 51.2, n = 75), while for 60 cm depth 0.7 mmhr^-1^ (0.2 - 1.2, n = 40) [@Kinner:2004]. For reference, a storm with 12.5 mmhr^-1^ rainfall intensity has a 0.2 probability of occurring in any given rainfall event.",
         "Based on throughfall data from [@Zimmermann:2010kzl] and precipitation data for BCI from STRI Physical Monitoring program, defined for precipitation events greater than 10 mm."
         )
         )
